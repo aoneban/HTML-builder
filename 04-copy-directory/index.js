c0125f3002d886e8071365c5c374d5c15f50492c
aoneban <torgregion1@gmail.com>
2023-05-08 14:05:36 +0200

    feat: add solve six task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,24 +2,24 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = path.join(__dirname, 'files');
-const destinationDir = path.join(__dirname, 'files-copy');
+const sourceDir_2 = path.join(__dirname, 'files');
+const destinationDir_2 = path.join(__dirname, 'files-copy');
 
-fs.mkdir(destinationDir, { recursive: true }, (err) => {
+fs.mkdir(destinationDir_2, { recursive: true }, (err) => {
   if (err) {
     console.error(`Error: ${err}`);
     return;
   }
 
-  fs.readdir(sourceDir, (err, files) => {
+  fs.readdir(sourceDir_2, (err, files) => {
     if (err) {
       console.error(`Error: ${err}`);
       return;
     }
 
     files.forEach((file) => {
-      const sourcePath = path.join(sourceDir, file);
-      const destinationPath = path.join(destinationDir, file);
+      const sourcePath = path.join(sourceDir_2, file);
+      const destinationPath = path.join(destinationDir_2, file);
 
       fs.copyFile(sourcePath, destinationPath, (err) => {
         if (err) {
---
+++ 06-build-page/index.js
@@ -0,0 +1,153 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const destinationDir = path.join(__dirname, 'project-dist');
+const sourceDir_2 = path.join(__dirname, 'assets');
+const destinationDir_2 = path.join(__dirname, 'project-dist', 'assets');
+const filePath = path.join(__dirname, 'template.html');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      return;
+    }
+    console.log(data);
+    const filePath = path.join(__dirname, 'project-dist', 'index.html');
+    const fileContent = data; 
+    fs.writeFile(filePath, fileContent, 'utf8', (err) => {
+        if (err) {
+          console.error(`Error: ${err}`);
+          return;
+        }
+      
+        console.log(`File ${filePath} created successfully.`);
+      });
+      const componentsDir = path.join(__dirname, 'components');
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(`Error: ${err}`);
+          return;
+        }
+        function readComponentFile(componentFile, placeholder) {
+          return new Promise((resolve, reject) => {
+            const componentFilePath = path.join(componentsDir, componentFile);
+            fs.readFile(componentFilePath, 'utf8', (err, componentData) => {
+              if (err) {
+                reject(`Error: ${err}`);
+                return;
+              }
+              data = data.replace(placeholder, componentData);
+              resolve();
+            });
+          });
+        }
+      
+        Promise.all([
+          readComponentFile('header.html', '{{header}}'),
+          readComponentFile('articles.html', '{{articles}}'),
+          readComponentFile('footer.html', '{{footer}}')
+        ])
+          .then(() => {
+            fs.writeFile(filePath, data, 'utf8', (err) => {
+                if (err) {
+                  console.error(`Error: ${err}`);
+                  return;
+                }
+              
+                console.log(`File ${filePath} created successfully.`);
+              }); 
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+  });
+
+function copyDirectoryRec(source, destination, callback) {
+    fs.mkdir(destination, { recursive: true }, (err) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+  
+      fs.readdir(source, (err, files) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+  
+        files.forEach((file) => {
+          const sourcePath = path.join(source, file);
+          const destinationPath = path.join(destination, file);
+  
+          fs.stat(sourcePath, (err, stats) => {
+            if (err) {
+              callback(err);
+              return;
+            }
+  
+            if (stats.isFile()) {
+              fs.copyFile(sourcePath, destinationPath, (err) => {
+                if (err) {
+                  callback(err);
+                } else {
+                  console.log(`Copied file: ${sourcePath}`);
+                }
+              });
+            } else if (stats.isDirectory()) {
+              copyDirectoryRec(sourcePath, destinationPath, callback);
+            }
+          });
+        });
+  
+        callback(null);
+      });
+    });
+  }
+  
+  copyDirectoryRec
+(sourceDir_2, destinationDir_2, (err) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+    } else {
+      console.log('Success');
+    }
+  });
+
+fs.mkdir(destinationDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      return;
+    } 
+    fs.readFile(path.join(__dirname, 'styles','header.css'), 'utf8', (err, data1) => {
+        if (err) {
+          console.error(`Error: ${err}`);
+          return;
+        }
+      
+        fs.readFile(path.join(__dirname, 'styles','main.css'), 'utf8', (err, data2) => {
+          if (err) {
+            console.error(`Error: ${err}`);
+            return;
+          }
+    
+          fs.readFile(path.join(__dirname, 'styles','footer.css'), 'utf8', (err, data3) => {
+            if (err) {
+              console.error(`Error: ${err}`);
+              return;
+            }
+      
+            const combinedData = data1 + data2 + data3;
+    
+            fs.writeFile(path.join(__dirname, 'project-dist','style.css'), combinedData, 'utf8', (err) => {
+              if (err) {
+                console.error(`Error: ${err}`);
+              } else {
+                console.log(`Success`);
+              }
+            });
+          });
+        });
+      });
+})
