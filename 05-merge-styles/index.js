5edefa1c9fb36c52357321e94d289c1203e08476
aoneban <torgregion1@gmail.com>
2024-01-24 11:36:04 +0100

    fix: fix some bugs in 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,50 +1,26 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-fs.readFile(
-  path.join(__dirname, 'styles', 'style-1.css'),
-  'utf8',
-  (err, data1) => {
-    if (err) {
-      console.error(`Error: ${err}`);
-      return;
-    }
+const stylesDir = path.join(__dirname, 'styles');
+const distDir = path.join(__dirname, 'project-dist');
+const bundlePath = path.join(distDir, 'bundle.css');
 
-    fs.readFile(
-      path.join(__dirname, 'styles', 'style-2.css'),
-      'utf8',
-      (err, data2) => {
-        if (err) {
-          console.error(`Error: ${err}`);
-          return;
-        }
-
-        fs.readFile(
-          path.join(__dirname, 'styles', 'style-3.css'),
-          'utf8',
-          (err, data3) => {
-            if (err) {
-              console.error(`Error: ${err}`);
-              return;
-            }
-
-            const combinedData = data1 + data2 + data3;
-
-            fs.writeFile(
-              path.join(__dirname, 'project-dist', 'bundle.css'),
-              combinedData,
-              'utf8',
-              (err) => {
-                if (err) {
-                  console.error(`Error: ${err}`);
-                } else {
-                  console.log(`Success`);
-                }
-              }
-            );
-          }
-        );
-      }
-    );
+async function readAndCombineStyles() {
+  try {
+    const files = await fs.readdir(stylesDir);
+    const cssFiles = files.filter((file) => path.extname(file) === '.css');
+    const readPromises = cssFiles.map(async (file) => {
+      const filePath = path.join(stylesDir, file);
+      return fs.readFile(filePath, 'utf8');
+    });
+    const stylesData = await Promise.all(readPromises);
+    const combinedData = stylesData.join('\n');
+    await fs.mkdir(distDir, { recursive: true });
+    await fs.writeFile(bundlePath, combinedData, 'utf8');
+    console.log('Success');
+  } catch (error) {
+    console.error('Error:', error);
   }
-);
+}
+
+readAndCombineStyles();
