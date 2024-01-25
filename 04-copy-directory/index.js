1afa94f4cf1549f2f06685e4b6a57b94ced6f7e1
aoneban <torgregion1@gmail.com>
2024-01-25 12:47:27 +0100

    fix: fix some bugs in 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const sourceDir = path.join(__dirname, 'files');
@@ -6,18 +6,18 @@ const resultDir = path.join(__dirname, 'files-copy');
 
 async function checkAndCreateDirectory(directoryPath) {
   try {
-    await fs.access(directoryPath, fs.constants.F_OK);
+    await fsPromises.access(directoryPath, fsPromises.constants.F_OK);
   } catch (error) {
-    await fs.mkdir(directoryPath, { recursive: true });
+    await fsPromises.mkdir(directoryPath, { recursive: true });
   }
 }
 
 async function removeFiles() {
   try {
-    const filesCopy = await fs.readdir(resultDir);
+    const filesCopy = await fsPromises.readdir(resultDir);
     const removePromises = filesCopy.map(async (file) => {
       const filePath = path.join(resultDir, file);
-      await fs.unlink(filePath);
+      await fsPromises.unlink(filePath);
     });
     await Promise.all(removePromises);
     console.log('Success');
@@ -28,11 +28,12 @@ async function removeFiles() {
 
 async function copyFiles() {
   try {
-    const files = await fs.readdir(sourceDir);
+    const files = await fsPromises.readdir(sourceDir);
     const copyPromises = files.map(async (file) => {
       const sourcePath = path.join(sourceDir, file);
       const destinationPath = path.join(resultDir, file);
-      await fs.copyFile(sourcePath, destinationPath);
+      const fileData = await fsPromises.readFile(sourcePath);
+      await fsPromises.writeFile(destinationPath, fileData);
       console.log(`Copied: ${sourcePath}`);
     });
     await Promise.all(copyPromises);
