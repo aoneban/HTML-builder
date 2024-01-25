e20a4a61e89bcf5a66ec25af3911e7e1eadab49e
aoneban <torgregion1@gmail.com>
2024-01-25 12:44:07 +0100

    fix: fix some bugs in 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const goalDir = path.join(__dirname, 'project-dist');
@@ -8,12 +8,12 @@ const filePath = path.join(__dirname, 'template.html');
 
 async function readComponentFiles(componentsDir) {
   try {
-    const files = await fs.readdir(componentsDir);
+    const files = await fsPromises.readdir(componentsDir);
 
     const componentProm = files.map(async (file) => {
       const placeholder = `{{${file.replace('.html', '')}}}`;
       const componentPath = path.join(componentsDir, file);
-      const componentData = await fs.readFile(componentPath, 'utf8');
+      const componentData = await fsPromises.readFile(componentPath, 'utf8');
       return { placeholder, componentData };
     });
 
@@ -27,13 +27,13 @@ async function loadComponents() {
   try {
     const componentsDir = path.join(__dirname, 'components');
     const components = await readComponentFiles(componentsDir);
-    let data = await fs.readFile(filePath, 'utf8');
+    let data = await fsPromises.readFile(filePath, 'utf8');
     components.forEach(({ placeholder, componentData }) => {
       data = data.replace(placeholder, componentData);
     });
     const indexPath = path.join(goalDir, 'index.html');
-    await fs.writeFile(indexPath, data, 'utf8');
-    console.log(`File ${indexPath} created width success.`);
+    await fsPromises.writeFile(indexPath, data, 'utf8');
+    console.log(`File ${indexPath} created with success.`);
   } catch (error) {
     throw new Error(`Error: ${error}`);
   }
@@ -41,15 +41,16 @@ async function loadComponents() {
 
 async function copyDirectoryRec(starting, goal) {
   try {
-    await fs.mkdir(goal, { recursive: true });
-    const files = await fs.readdir(starting);
+    await fsPromises.mkdir(goal, { recursive: true });
+    const files = await fsPromises.readdir(starting);
     await Promise.all(
       files.map(async (file) => {
         const startingPath = path.join(starting, file);
         const goalPath = path.join(goal, file);
-        const stats = await fs.stat(startingPath);
+        const stats = await fsPromises.stat(startingPath);
         if (stats.isFile()) {
-          await fs.copyFile(startingPath, goalPath);
+          const fileData = await fsPromises.readFile(startingPath);
+          await fsPromises.writeFile(goalPath, fileData);
           console.log(`Copy: ${startingPath}`);
         } else if (stats.isDirectory()) {
           await copyDirectoryRec(startingPath, goalPath);
@@ -64,14 +65,14 @@ async function copyDirectoryRec(starting, goal) {
 async function createStyleFile() {
   try {
     const stylesDir = path.join(__dirname, 'styles');
-    const styleFiles = await fs.readdir(stylesDir);
+    const styleFiles = await fsPromises.readdir(stylesDir);
     const stylePromises = styleFiles.map(async (file) => {
       const stylePath = path.join(stylesDir, file);
-      return await fs.readFile(stylePath, 'utf8');
+      return await fsPromises.readFile(stylePath, 'utf8');
     });
 
     const combinedStyleData = (await Promise.all(stylePromises)).join('\n');
-    await fs.writeFile(
+    await fsPromises.writeFile(
       path.join(__dirname, 'project-dist', 'style.css'),
       combinedStyleData,
       'utf8',
@@ -88,7 +89,7 @@ async function main() {
     console.log('Success');
     await loadComponents();
     await createStyleFile();
-    await fs.mkdir(goalDir, { recursive: true });
+    await fsPromises.mkdir(goalDir, { recursive: true });
     console.log('success');
   } catch (error) {
     console.error(`Error: ${error}`);
